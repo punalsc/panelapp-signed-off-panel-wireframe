e8e2a304beea1e1697cf9bda666e21ed546571da
Punal Chotrani <punalsc@gmail.com>
2020-07-29 12:34:45 +0100

    css updates

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ fetch(
                 <div class='titles'>
                     <div class='row'>
                         <div class='twelve columns'>
-                            <h6><b>Name:</b> ${data.name}</h6>
-                            <h6><b>Version:</b> ${data.version}</h6>
+                            <h4><b>${data.name} (Version: ${
+        data.version
+      })</b></h4>
                             <h6><b>Signed off date:</b> ${data.signed_off}</h6>
                             <h6><b>Panel types:</b> ${data.types
                               .map((item) => item.name)
@@ -42,23 +43,23 @@ fetch(
                 </div>
                 <div class='genes'>
                         <div class='row gene-data'>
-                                <div class='two columns'><b>Confidence level</b></div>
-                                <div class='two columns'><b>Entity name</b></div>
+                                <div class='one columns'><b>Level</b></div>
+                                <div class='one columns'><b>Entity</b></div>
                                 <div class='three columns'><b>Mode of inheritance</b></div>
-                                <div class='two columns'><b>Mode of pathogenicity</b></div>
-                                <div class='two columns'><b>Tags</b></div>
+                                <div class='three columns'><b>Mode of pathogenicity</b></div>
+                                <div class='three columns'><b>Tags</b></div>
                         </div>
                         ${data.genes
                           .map(
                             (gene) =>
                               `
                               <div class='row gene-data'>
-                                <div class='two columns'><span class='confidence-level-badge confidence-level-badge-${confidenceLevel(
+                                <div class='one columns'><span class='confidence-level-badge confidence-level-badge-${confidenceLevel(
                                   gene.confidence_level
                                 ).toLowerCase()}'>${confidenceLevel(
                                 gene.confidence_level
                               )}</span></div>
-                                <div class='two columns'><b>${
+                                <div class='one columns'><b>${
                                   gene.entity_name
                                 }</b></div>
                                 <div class='three columns'>${
@@ -66,12 +67,12 @@ fetch(
                                     ? "N/A"
                                     : gene.mode_of_inheritance
                                 }</div>
-                                <div class='two columns'>${
+                                <div class='three columns'>${
                                   gene.mode_of_pathogenicity === ""
                                     ? "N/A"
                                     : gene.mode_of_pathogenicity
                                 }</div>
-                            <div class='two columns'>${
+                            <div class='three columns'>${
                               gene.tags.length === 0
                                 ? "N/A"
                                 : gene.tags
